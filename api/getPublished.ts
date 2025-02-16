543e05c400f8a9cbcfccf1fab54e690767bd5674
AsherMorse <ashermorse@icloud.com>
2025-02-16 16:03:09 -0600

    Update database connection environment variables in API files

--- api/getPublished.ts
+++ api/getPublished.ts
@@ -1,62 +1,62 @@
 import knex, { type Knex } from 'knex';
 
 type PublishedNote = {
-	id: string,
-	content: string,
-	author: string,
-	client_id: string,
-	modified: string,
+  id: string,
+  content: string,
+  author: string,
+  client_id: string,
+  modified: string,
 }
 
 const k: Knex = knex({
-	client: 'pg',
-	connection: {
-		host: process.env.PG_HOST,
-		user: process.env.PG_USER,
-		password: process.env.PG_PASSWORD,
-		database: process.env.PG_DB,
-	},
+  client: 'pg',
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+  },
 })
 
 const table = 'published'
 
 function decode(str: string) {
-	return Buffer.from(str, 'base64url').toString('utf8');
+  return Buffer.from(str, 'base64url').toString('utf8');
 }
 
 export async function GET(req: Request) {
-	return await getById(req);
+  return await getById(req);
 }
 
 async function getById(req) {
-	const params = new URL(req.url).searchParams;
-	const id = params.get('id');
+  const params = new URL(req.url).searchParams;
+  const id = params.get('id');
 
-	if (!id) {
-		return new Response('ID is empty', {
-			status: 400,
-		})
-	}
+  if (!id) {
+    return new Response('ID is empty', {
+      status: 400,
+    })
+  }
 
-	const note = await getNote(id)
+  const note = await getNote(id)
 
-	if (!note) {
-		return new Response('Note not found', {
-			status: 404,
-		})
-	}
+  if (!note) {
+    return new Response('Note not found', {
+      status: 404,
+    })
+  }
 
-	return new Response(JSON.stringify({
-		content: decode(note.content),
-		clientId: note.client_id
-	}), {
-		status: 200, statusText: "Returning note",
-	})
+  return new Response(JSON.stringify({
+    content: decode(note.content),
+    clientId: note.client_id
+  }), {
+    status: 200, statusText: "Returning note",
+  })
 }
 
 async function getNote(id: string): Promise<PublishedNote> {
-	return (await k<PublishedNote>(table)
-		.select()
-		.where('id', id)
-		.first())
+  return (await k<PublishedNote>(table)
+    .select()
+    .where('id', id)
+    .first())
 }
